88cb661ce33f26fabb46217169db5dd70cdc5a47
Splashcoder <Splashcoder@yandex.ru>
2021-10-17 16:51:59 +0300

    Commit project

---
+++ README-.md
@@ -0,0 +1,3 @@
+# React-ts-sass-mui-site-starter
+React Website starter!
+All you need for website creating already implemented: React 17, Webpack 5, TypeScript 4, sass, MUI - material user interface
---
+++ src/components/Header.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+interface HeaderProps {
+  sections: ReadonlyArray<{
+    title: string;
+    url: string;
+  }>;
+  title: string;
+}
+
+export default function Header(props: HeaderProps) {
+  const { sections, title } = props;
+
+  const top100Films = [
+    { label: 'The Shawshank Redemption', year: 1994 },
+    { label: 'The Godfather', year: 1972 },
+    { label: 'The Godfather: Part II', year: 1974 },
+    { label: 'The Dark Knight', year: 2008 },
+    { label: '12 Angry Men', year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: 'Pulp Fiction', year: 1994 }
+  ];
+
+  return (
+    <React.Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Button size="small">Subscribe</Button>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+          {title}
+        </Typography>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <Autocomplete
+            freeSolo
+            id="free-solo-2-demo"
+            disableClearable
+            options={top100Films.map((option) => option.label)}
+            renderInput={(params) => (
+            <TextField
+                {...params}
+                label="Search input"
+                InputProps={{
+                ...params.InputProps,
+                type: 'search',
+                }}
+            />
+            )}
+        />
+        <Button variant="outlined" size="small">
+          Sign up
+        </Button>
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+      >
+        {sections.map((section) => (
+          <Link
+            color="inherit"
+            noWrap
+            key={section.title}
+            variant="body2"
+            href={section.url}
+            sx={{ p: 1, flexShrink: 0 }}
+          >
+            {section.title}
+          </Link>
+        ))}
+      </Toolbar>
+    </React.Fragment>
+  );
+}
